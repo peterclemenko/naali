7dfb8cd55d284a632b5cf8b03e7087c826e09a25
Erno Kuusela <erno@playsign.net>
2011-10-28 13:19:46 +0300

    small cleanups in t1->t2 txml converter

--- tools/tundra2-txml-converter.py
+++ tools/tundra2-txml-converter.py
@@ -2,51 +2,35 @@ import sys
 import os
 
 """
-    For this script to work you need the jack.mesh from /media/models in /data/assets
+    For this script to work you need to copy Jack.mesh from bin/media/models
+    to bin/data/assets
 
-    You will have to manually put your txml file name down in the __main__ func to:
-    --> fileName = "putFileNameHere.txml" <--
-    
     1. Run this script on your tundra 1.x txml
-    2. This will create a new file with "_" appended before the .txml extension.
+    2. This will create a new file with "_" appended before the .txml
+    extension.
     3. Open this file in your tundra 2.x server
     4. Find the "Jack" entity, he will be lurking at pos 0,0,0.
-    5. Shift+E for entity editor, select jack, if you dont see the manip visual aids, hit "tilde" (next to "1" key)
-    6. Open Placeable component and rotate how you like, the whole scene will rotate with jack. Seems that -90 to x will do the trick.
+    5. Shift+E for entity editor, select jack, if you dont see the
+    manip visual aids, hit "tilde" (next to "1" key)
+    6. Open Placeable component and rotate how you like, the whole
+    scene will rotate with jack. Seems that -90 to x will do the
+    trick.
     7. Shitf+S for scene editor, right click -> save scene as...
 
     Problems with this technique
-    * You cannot remove or edit the grandparent jacks placeable component
-      scale needs to be 1,1,1 or it will scale the whole scene (but might be handy if you want to do this)
+    * You cannot remove or edit the grandparent jacks placeable
+      component scale needs to be 1,1,1 or it will scale the whole
+      scene (but might be handy if you want to do this)
     * Jack grandparent needs to be there always or the scene will flip back.
 
-    There is also some experimental placeable Transform manipulations that you can do to the txml, but it wont work
-    propably on any scene as everything needs a common pivot point for the rotation.
+    There is also some experimental placeable Transform manipulations
+    that you can do to the txml, but it wont work propably on any
+    scene as everything needs a common pivot point for the rotation.
 
-    So this is a temp hack. Proper way is to export your modelfs from eg. blender with the correct axis flip built in
-    and import again.
+    So this is a temp hack. Proper way is to export your modelfs from
+    eg. blender with the correct axis flip built in and import again.
     
 """
-
-def getFileContent(filePath):
-    try:
-        f = open(filePath, 'r')
-        c = f.read()
-        f.close()
-        return c
-    except IOError as e:
-        print "IOError on input file:", filePath
-        print e
-    return None
-
-def saveNewContent(filePath, newContent):
-    try:
-        f = open(filePath, "w+")
-        f.write(newContent)
-        f.close()
-    except IOError as e:
-        print "IOError on writing to file:", filePath
-        print e
 
 class Transform:
 
@@ -83,34 +67,38 @@ class Transform:
         line += "," + self.rot["x"] + "," + self.rot["y"] + "," + self.rot["z"]
         line += "," + self.scale["x"] + "," + self.scale["y"] + "," + self.scale["z"]
         return line
+
+def usage_fail(errmsg):
+    sys.stderr.write(errmsg + "\n")
+    sys.stderr.write("Usage: tundra2-txml-converter.py -i|--input=inputTXML [-o|--output=outputTXML]\n")
+    sys.exit(1)
         
-if __name__ == "__main__":
+def main():
+    import getopt
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:o:", ["input=", "output="])
+    except getopt.GetoptError, err:
+        usage_fail(str(err))
+
+    newFileName = fileName = None
+    for o, a in opts:
+        if o in ("-i", "--input"):
+            fileName = a
+        elif o in ("-o", "--output"):
+            newFileName = a
+
+    if not fileName:
+        usage_fail("input filename missing")
+
+    if not newFileName:
+        newFileName = fileName[:fileName.index(".txml")] + "_.txml"
 
     try:
-        import getopt
-        try:
-            opts, args = getopt.getopt(sys.argv[1:], "i:o:", ["input=", "output="])
-        except getopt.GetoptError, err:
-            # print help information and exit:
-            print str(err)
-            print "Usage: tundra2-txml-converter.py [-i|--input=inputTXML] [-o|--output=outputTXML]"
-            sys.exit(2)
-        # Set defaults first:
-        fileName = "putFileNameHere.txml"
-        newFileName = fileName[:fileName.index(".txml")] + "_.txml"
-        for o, a in opts:
-            if o in ("-i", "--input"):
-                fileName = a
-            elif o in ("-o", "--output"):
-                newFileName = a
-    except ImportError:
-        fileName = "putFileNameHere.txml"
-        newFileName = fileName[:fileName.index(".txml")] + "_.txml"
+        c = open(fileName).read()
+    except IOError, what:
+        sys.exit("error reading input file %s: %s" % (fileName, what))
 
-    c = getFileContent(fileName)
-    if c == None: sys.exit(2)
     lines = c.splitlines(True)
-
     parentName = "GeneratedGrandParentEntity"
 
     parentEntXml =  """ <entity id="1">
@@ -175,24 +163,12 @@ if __name__ == "__main__":
         else:
             out += line
             
-        """
-        if line.count("name=\"Transform\"") <= 0:
-            out += line
-            continue
+    try:
+        f = open(newFileName, "w")
+        f.write(out)
+        f.close()
+    except IOError, what:
+        sys.exit("error writing to file %s: %s" % (newFileName, what))
 
-        start = line.find("\"")
-        if start == -1:
-            out += line
-            continue
-        end = line.find("\"", start+1)
-
-        value = line[start+1:end]
-
-        t = Transform(value)
-        t.flip(t.rot, "y", "z")
-        newValue = t.getNewValue()
-
-        out += line.replace(value, newValue)
-        """
-
-    saveNewContent(newFileName, out)
+if __name__ == "__main__":
+    main()

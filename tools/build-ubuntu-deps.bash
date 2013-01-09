83d14591cd23f671b8231e74469685ca54919b9a
Peter <aoi@pciii-linux-desktop.(none)>
2013-01-09 15:35:12 -0500

    Updated boost to 1.48-all-dev to fix issues I've had on my end with apt wanting to remove a lot of my installs due to conflicts.

--- tools/build-ubuntu-deps.bash
+++ tools/build-ubuntu-deps.bash
@@ -44,7 +44,7 @@ export TUNDRA_PYTHON_ENABLED=TRUE
 if lsb_release -c | egrep -q "lucid|maverick|natty|oneiric|precise|maya|lisa|katya|julia|isadora|quantal|nadia" && tty >/dev/null; then
         which aptitude > /dev/null 2>&1 || sudo apt-get install aptitude
 	sudo aptitude -y install git-core python-dev libogg-dev libvorbis-dev \
-	 build-essential g++ libboost-all-dev libois-dev \
+	 build-essential g++ libboost1.48-all-dev libois-dev \
 	 ccache libqt4-dev python-dev freeglut3-dev \
 	 libxml2-dev cmake libalut-dev libtheora-dev ed \
 	 liboil0.3-dev mercurial unzip xsltproc libois-dev libxrandr-dev \
